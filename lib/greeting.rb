e193c84ce971759e69cf41986f3b98746ecdfb25
adamdraznin@gmail.com <adamdraznin@gmail.com>
2018-08-19 18:50:38 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here!
 learn spec/01_greeting_spec.rb
-puts "Hello #{name}. It's niceto meet you."
+puts "Hello #{name}. It's nice to meet you."
 name = gets.strip
 greeting(name)
