7a34bad155f971b6cc273290f024c6c58c8e987c
adamdraznin@gmail.com <adamdraznin@gmail.com>
2018-08-19 18:42:57 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,3 +1,5 @@
 # code the #greeting method here!
 learn spec/01_greeting_spec.rb
-puts "Hi I'm #{name}, what's your name."
+puts "Hi I'm #{name}, what's your name?"
+name = gets.strip
+greeting(HAL)
